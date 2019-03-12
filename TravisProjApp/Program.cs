831cd2adaf4a2e09bfbe28f749abc763eeae5590
Hilary Sasso-Schleh <hsassoschleh@pillartechnology.com>
2019-03-12 15:26:59 -0400

    created sln and project files

---
+++ TravisProjApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TravisProjApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
